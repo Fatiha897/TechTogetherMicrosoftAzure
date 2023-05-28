3e870bbffccbe367820c6e99e6009b0a5135a9e9
Dayana Alejandro <dayana200120@gmail.com>
2023-05-28 07:37:49 -0400

    modified html pages to show on flask

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from .travelapi import *
 
 app = Flask(__name__)
+class Trip:
+    def __init__(self):
+        self.name = ""
+        self.location = ""
+        self.budget = ""
+        self.stay = ""
+
+trip = Trip()
+
 
 @app.route("/")
-@app.route("/marcosolo")
-def hello_world():
-    return render_template('marcosolo.html')
+@app.route("/home", methods =["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("NamE")
+        trip.name = name
+        print("HERE", trip.name)
+    return render_template('home.html')
 
+
+@app.route("/planatrip", methods =["GET", "POST"])
+def transport():
+    # return "hotels"+ str(hotels_lst)
+    return render_template('transport.html')
+
+
+@app.route("/hotels", methods =["GET", "POST"])
+def hotels():
+    # return "hotels"+ str(hotels_lst)
+    return render_template('hotel.html') # hotels = str(foodnearby(100)))
+
+
+@app.route("/food", methods =["GET", "POST"])
+def food():
+    # return "hotels"+ str(hotels_lst)
+    return render_template('food.html')
+
+
+@app.route("/summary", methods =["GET", "POST"])
+def summary():
+    # return "hotels"+ str(hotels_lst)
+    return render_template('summary.html')
+
+
+@app.route("/startTrip", methods =["GET", "POST"])
+def end():
+    # return "hotels"+ str(hotels_lst)
+    return render_template('startTrip.html')
